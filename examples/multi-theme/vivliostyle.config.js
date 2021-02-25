b6408934ff56b70e7e61c5c5eb223f31e2dc3f6d
Ayumu Takai <atakai@hackwork.jp>
2021-02-25 23:40:15 +0900

    feat: support multi themes at entry field in vivliostyle.config.js

--- examples/multi-theme/vivliostyle.config.js
+++ examples/multi-theme/vivliostyle.config.js
@@ -4,6 +4,10 @@ module.exports = {
   language: 'ja',
   size: 'A5',
   theme: ['@vivliostyle/theme-bunko','sub-theme.css'],
-  entry: 'bunko.md',
+  entry: ['bunko.md',
+          {
+            path: './yume.md',
+            theme: ['@vivliostyle/theme-bunko','sub-theme2.css'],
+          }],
   output: 'bunko.pdf',
 };

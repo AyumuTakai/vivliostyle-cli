38f7909ac35495cfe9394143efadf62d04cccc63
Ayumu Takai <atakai@hackwork.jp>
2021-02-27 20:29:50 +0900

    chore: enable textlint plugin

--- examples/multi-theme/vivliostyle.config.js
+++ examples/multi-theme/vivliostyle.config.js
@@ -3,11 +3,13 @@ module.exports = {
   author: 'spring-raining',
   language: 'ja',
   size: 'A5',
-  theme: ['@vivliostyle/theme-bunko','sub-theme.css','./emojicat','./emojihand'],
+  theme: ['@vivliostyle/theme-bunko','sub-theme.css','./emojicat','./emojihand','./textlint'],
   entry: ['bunko.md',
           {
             path: './yume.md',
             theme: ['@vivliostyle/theme-bunko','sub-theme2.css'],
-          }],
+          },
+  ],
   output: 'bunko.pdf',
+  workspaceDir: ".vivliostyle",
 };

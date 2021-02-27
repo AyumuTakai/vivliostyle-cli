26822623fb3bc2f42506357acd3cdad1e7975059
Ayumu Takai <atakai@hackwork.jp>
2021-02-27 20:28:12 +0900

    feat: imprement preprocess

--- src/markdown.ts
+++ src/markdown.ts
@@ -9,13 +9,29 @@ export interface VSFile extends VFile {
   };
 }
 
+export type PreProcess = (filepath: string, contents: string) => string;
+
+function getContents(
+  filepath: string,
+  preprocess: PreProcess[] | undefined,
+): string {
+  let contents = fs.readFileSync(filepath, 'utf8');
+  if (contents && preprocess) {
+    for (const proc of preprocess) {
+      contents = proc(filepath, contents);
+    }
+  }
+  return contents;
+}
+
 export function processMarkdown(
   filepath: string,
   options: StringifyMarkdownOptions = {},
+  preprocess: PreProcess[] | undefined = undefined,
 ): VSFile {
   const vfm = VFM(options);
   const processed = vfm.processSync(
-    vfile({ path: filepath, contents: fs.readFileSync(filepath, 'utf8') }),
+    vfile({ path: filepath, contents: getContents(filepath, preprocess) }),
   ) as VSFile;
   return processed;
 }

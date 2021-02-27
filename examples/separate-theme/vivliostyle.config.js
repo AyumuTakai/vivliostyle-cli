4eadbc793c793605558194e2af9aa50675bf0b36
Ayumu Takai <atakai@hackwork.jp>
2021-02-27 20:01:42 +0900

    add: seprate-theme sample

---
+++ examples/separate-theme/vivliostyle.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: 'Draft with the separate theme',
+  author: 'Jhon Doe',
+  language: 'ja',
+  size: 'A4',
+  theme: ['A4book.css','2column.css',/*'check.css'*/],
+  entry: 'bunko.md',
+  output: 'book.pdf',
+};

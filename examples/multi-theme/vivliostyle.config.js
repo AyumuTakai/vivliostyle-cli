bac0278996ae55b20e01f4fb28a2a7834594f302
Ayumu Takai <atakai@hackwork.jp>
2021-03-02 07:47:54 +0900

    chore: reformat vivliostyle.config.js

--- examples/multi-theme/vivliostyle.config.js
+++ examples/multi-theme/vivliostyle.config.js
@@ -3,12 +3,24 @@ module.exports = {
   author: 'spring-raining',
   language: 'ja',
   size: 'A5',
-  theme: ['@vivliostyle/theme-bunko','sub-theme.css','./emojicat','./emojihand','./textlint'],
-  entry: ['bunko.md',
-          {
-            path: './yume.md',
-            theme: ['@vivliostyle/theme-bunko','sub-theme2.css'],
-          },
+  theme: [
+    '@vivliostyle/theme-bunko',
+    // 'sub-theme.css',
+    // 'openJTalk',
+    // 'textlint',
+    // 'emojicat',
+    // 'emojihand'
+  ],
+  entry: [
+    'bunko.md',
+    {
+      path: './yume.md',
+      theme: [
+        '@vivliostyle/theme-bunko',
+        // 'sub-theme2.css',
+        // 'textlint'
+      ],
+    },
   ],
   output: 'bunko.pdf',
   workspaceDir: ".vivliostyle",

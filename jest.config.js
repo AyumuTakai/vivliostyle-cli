f4c07f9c41365554a06940e875f5c25b2e3621e4
Ayumu Takai <atakai@hackwork.jp>
2021-03-07 01:45:52 +0900

    fix: suppress warnings

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: './tests/fixtures/node.js',
   testMatch: ['**/src/__tests__/*.+(ts|tsx|js)', '**/tests/*.test.(ts|tsx|js)'],
-  testPathIgnorePatterns: ['/dist/'],
+  testPathIgnorePatterns: ['/dist/', '/.vivliostyle/'],
 };
